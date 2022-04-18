5965fd7284aa03dfb646790e580850a4951288d3
vort3 <sherigulam@gmail.com>
2022-04-18 17:44:03 +0000

    Use parsing by css selectors instead of visible page text, removed all re parsing, added explicit waits for page loading

---
+++ cellkeys.py
@@ -0,0 +1,12 @@
+cellkeys = {
+    0: "icon",
+    1: "departure",
+    2: "event",
+    3: "arrival",
+    4: "crew",
+    5: "info",
+    6: "comment",
+    7: "confirm",
+    8: "reports",
+    }
+
--- mer2gc.py
+++ mer2gc.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python3
 
 from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
-from bs4 import BeautifulSoup
 from google.oauth2 import service_account
 from gcsa.google_calendar import GoogleCalendar
 from gcsa.event import Event
+from bs4 import BeautifulSoup
 import locations
+import cellkeys
 import datetime
 import logging
 import json
 import sys
 import os
-import re
 
 
-def get_pagetext(url, login, password):
+def get_pagesource(url, login, password):
     opt = Options()
     opt.headless = True
     opt.add_argument("-private")
@@ -25,98 +27,67 @@ def get_pagetext(url, login, password):
 
     with webdriver.Firefox(options=opt, service=s) as drv:
         drv.get(url)
+        WebDriverWait(drv, 30).until(EC.visibility_of_element_located(
+                                    (By.CLASS_NAME, "z-button")))
         drv.find_elements(By.CLASS_NAME, "z-textbox")[0].send_keys(login)
         drv.find_elements(By.CLASS_NAME, "z-textbox")[1].send_keys(password)
         drv.find_element(By.CLASS_NAME, "z-button").click()
         drv.get(url + "/web.meridian/workplan.zul")
-        pagetext = BeautifulSoup(drv.page_source, "html.parser").get_text()
-        logging.debug(pagetext)
-        return pagetext
+        WebDriverWait(drv, 30).until(EC.visibility_of_element_located(
+                                    (By.CLASS_NAME, "fio")))
+        src = BeautifulSoup(drv.page_source, "html.parser")
+        logging.debug(src)
+        return src
 
 
-def get_username(pagetext, alternative=False):
-    if not alternative:
-        # For when bs4 eats all newlines
-        pattern = "\s(?P<username>[A-Z]+ [A-Z]+)\s+.*?7"
-    elif alternative:
-        logging.info("Using alternative pattern to parse username.")
-        pattern = "\n(?P<username>[A-Z]+ [A-Z]+)\n"
+def get_events(src):
+    events = []
 
-    match = re.search(pattern, pagetext)
-    if match is None:
-        return
-    return match.group(1)
-
-
-def get_legs(pagetext, alternative=False):
-    if not alternative:
-        # For when bs4 eats all newlines:
-        pattern = ( "(?:(?P<date>\d{2}\.\d{2}\.\d{4})\s*"
-                    "\([A-Z][a-z]{2}\)\n*)?\s*"
-                    "(?P<start>\d{2}:\d{2})"
-                    "(?P<dep>[A-Z]{3})\s*"
-                    "(?P<fnumber>DV\d{3,4}D?).*?"
-                    "(?P<end>\d{2}:\d{2})"
-                    "(?:\s*\(\+1\))?"
-                    "(?P<dest>[A-Z]{3})"
-                    "(?P<crew>.*?):" )
-    elif alternative:
-        # When each cell is on a separate line:
-        logging.info("Using alternative pattern to parse legs.")
-        pattern = ( "(?:(?P<date>\d{2}\.\d{2}\.\d{4})\s"
-                    "\([A-Z][a-z]{2}\)\n{3,4})?"
-                    "(?P<start>\d{2}:\d{2})"
-                    "(?P<dep>[A-Z]{3})\n"
-                    "(?P<fnumber>DV\d{3,4}D?).*\n"
-                    "(?P<end>\d{2}:\d{2})"
-                    "(?:\s\(\+1\))?"
-                    "(?P<dest>[A-Z]{3})"
-                    "\n.*\n"
-                    "(?P<crew>.*)" )
-
-    legs = [m.groupdict() for m in re.finditer(pattern, pagetext)]
+    rows = src.select(".z-listitem")
+    for row in rows:
+        event = {}
+        for i, cell in enumerate(row.select(".z-listcell-content")):
+            content = []
+            for span in cell.select("span"):
+                if "display:none" not in span.get("style", ""):
+                    content.append(span.text)
+            event[cellkeys.cellkeys[i]] = content
+        events.append(event)
     
-    for i, leg in enumerate(legs):
-        if leg["date"] == None:
-            leg["date"] = legs[i-1]["date"]
+    for i, event in enumerate(events):
+        if len(event) == 3:
+            event["date"] = event["departure"][0].split()
+        else:
+            event["date"] = events[i - 1]["date"]
     
-    return legs
+    return list(filter(lambda i: len(i) != 4, events))
 
 
-def get_reserves(pagetext, alternative=False):
-    if not alternative:
-        # For when bs4 eats all newlines:
-        pattern = ( "(?:(?P<date>\d{2}\.\d{2}\.\d{4})\s*"
-                    "\([A-Z][a-z]{2}\)\n*)?\s*"
-                    "(?P<start>\d{2}:\d{2})\s*"
-                    "(?P<title>Рез.*?)\s*"
-                    "(?P<end>\d{2}:\d{2})" )
-    elif alternative:
-        logging.info("Using alternative pattern to parse reserves.")
-        logging.info("Warning: Not implemented yet")
-        pattern = ""
-
-    reserves = [m.groupdict() for m in re.finditer(pattern, pagetext)]
+def process_event(event, calendar):
+    if len(event["event"]) == 0:
+        # ToDo: handle trainings better
+        event["event"] = ["TRAINING: " + event["comment"][-1]]
+    fnumber = event["event"][0]
+    dep = event["departure"][-1]
+    arr = event["arrival"][-1]
     
-    for i, reserve in enumerate(reserves):
-        if reserve["date"] == None:
-            reserve["date"] = reserves[i-1]["date"]
+    if "Passenger on task" in event["info"]:
+        title = f"{dep}-{arr} {fnumber} (PAX)"
+    elif "TRAINING" in event["event"][0]:
+        title = fnumber
+    else:
+        title = f"{dep}-{arr} {fnumber}"
     
-    return reserves
-
-
-def process_leg(leg, calendar, username):
-    fnumber = leg["fnumber"]
-    dep = leg["dep"]
-    dest = leg["dest"]
-    title = f"{dep}-{dest} {fnumber} (PAX)" if f"{username} [pax]" in leg["crew"] \
-                                            else f"{dep}-{dest} {fnumber}"
-    start = datetime.datetime.strptime(leg["date"]+leg["start"], "%d.%m.%Y%H:%M")
+    start = datetime.datetime.strptime(
+            event["date"][0] + event["departure"][0], "%d.%m.%Y%H:%M")
     start = start.replace(tzinfo=datetime.timezone.utc)
-    end = datetime.datetime.strptime(leg["date"]+leg["end"], "%d.%m.%Y%H:%M")
-    end = end.replace(tzinfo=datetime.timezone.utc)
-    if end < start:
+    end   = datetime.datetime.strptime(
+            event["date"][0] + event["arrival"][0], "%d.%m.%Y%H:%M")
+    end   = end.replace(tzinfo=datetime.timezone.utc)
+
+    if " (+1)" in event["arrival"]:
         end += datetime.timedelta(days=1)
+    
     flight = f"{start.date().isoformat()} {fnumber}"
     logging.info(f"Current flight:\t{flight}")
     existing = list(calendar.get_events(time_min=start.date(),
@@ -135,43 +106,7 @@ def process_leg(leg, calendar, username):
         return
     event = Event(title, start, end)
     event.timezone = "Etc/UTC"
-    event.location = locations.locations.get(dest, "Unknown location")
-    event.add_popup_reminder(minutes_before_start=180)
-    event.description = "Created by Meridian2GC"
-    print(f"Generated event:\t{title}")
-    calendar.add_event(event)
-    logging.info("Event added")
-
-
-def process_reserve(reserve, calendar):
-    title = "RESERVE"
-    start = datetime.datetime.strptime(reserve["date"]+reserve["start"], "%d.%m.%Y%H:%M")
-    start = start.replace(tzinfo=datetime.timezone.utc)
-    end = datetime.datetime.strptime(reserve["date"]+reserve["end"], "%d.%m.%Y%H:%M")
-    end = end.replace(tzinfo=datetime.timezone.utc)
-    if end < start:
-        end += datetime.timedelta(days=1)
-    existing = list(calendar.get_events(time_min=start.date(),
-                    time_max=end.date(), query=title, timezone="Etc/UTC"))
-    if existing:
-        if existing[0].start == start and existing[0].end == end:
-            logging.info("Start and end times match. Skipping…")
-            return
-        print("Updating event…",
-              f"{str(existing[0].start)[:16]} - {str(existing[0].end)[:16]}",
-              f"---> {str(start)[:16]} - {str(end)[:16]}")
-        existing[0].start = start
-        existing[0].end = end
-        calendar.update_event(existing[0])
-        return
-    event = Event(title, start, end)
-    event.timezone = "Etc/UTC"
-    
-    last_event = list(calendar.get_events(time_min=datetime.datetime.now() -
-                             datetime.timedelta(days=30),
-                             time_max=start))[-1]
-    event.location = last_event.location
-    
+    event.location = locations.locations.get(arr, "Unknown location")
     event.add_popup_reminder(minutes_before_start=180)
     event.description = "Created by Meridian2GC"
     print(f"Generated event:\t{title}")
@@ -190,11 +125,6 @@ def main():
                   SERVICE_ACCOUNT_FILE, scopes=SCOPES)
     calendar = GoogleCalendar(conf['calendar'], credentials=credentials)
 
-    if "-a" in sys.argv:
-        alternative = True
-    else:
-        alternative = False
-
     if "-v" in sys.argv:
         logging.basicConfig(format="[%(levelname)s]\t%(message)s",
                             level=logging.DEBUG, stream=sys.stdout)
@@ -204,23 +134,11 @@ def main():
     else:
         logging.basicConfig(format="[%(levelname)s]\t%(message)s",
                             level=logging.INFO, stream=sys.stdout)
-        
-    for i in range(10):
-        pagetext = get_pagetext(conf['url'], conf['login'], conf['password'])
-        username = get_username(pagetext, alternative=alternative)
-        if username != None:
-            break
-    else:
-        logging.warning("Unable to load page text.")
-        return
 
-    legs = get_legs(pagetext, alternative=alternative)
-    for leg in legs:
-        process_leg(leg, calendar, username)
-    
-    reserves = get_reserves(pagetext, alternative=alternative)
-    for reserve in reserves:
-        process_reserve(reserve, calendar)
+    src = get_pagesource(conf['url'], conf['login'], conf['password'])
+    events = get_events(src)
+    for event in events:
+        process_event(event, calendar)
 
 
 if __name__ == "__main__":
